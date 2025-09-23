cbf2c7c26a5cd02f05bae0db9754a60ac1c8c1eb
Datnq <101233448+Senkiro@users.noreply.github.com>
2025-09-23 21:56:09 +0700

    detail page 23/09/2025

---
+++ TodoDetailPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiToDoApp;
+
+public partial class TodoDetailPage : ContentPage
+{
+    public TodoDetailPage(TodoItem item)
+    {
+        InitializeComponent();
+
+        TitleLabel.Text = item.Title;
+        StatusLabel.Text = item.IsCompleted ? "✅ Đã hoàn thành" : "❌ Chưa hoàn thành";
+        CreatedAtLabel.Text = item.CreatedAt.ToString("dd/MM/yyyy HH:mm");
+    }
+}
+
--- TodoItem.cs
+++ TodoItem.cs
@@ -1,14 +1,63 @@
-﻿namespace MauiToDoApp;
+﻿using System.ComponentModel;
 
-public class TodoItem
+namespace MauiToDoApp;
+
+public class TodoItem : INotifyPropertyChanged
 {
+<<<<<<< HEAD
+    private string _title;
+    public string Title
+    {
+        get => _title;
+        set
+        {
+            if (_title != value)
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+    }
+
+=======
     public string Title { get; set; }
-    public bool IsCompleted { get; set; }
+>>>>>>> ecfcb6d406a6bc260cc3e57a899bb2427741552f
+    public DateTime CreatedAt { get; set; }
+
+    private bool _isCompleted;
+    public bool IsCompleted
+    {
+        get => _isCompleted;
+        set
+        {
+            if (_isCompleted != value)
+            {
+                _isCompleted = value;
+<<<<<<< HEAD
+                OnPropertyChanged(nameof(IsCompleted));
+=======
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsCompleted)));
+>>>>>>> ecfcb6d406a6bc260cc3e57a899bb2427741552f
+                OnIsCompletedChanged?.Invoke();
+            }
+        }
+    }
+
+    public Action OnIsCompletedChanged { get; set; }
 
     public TodoItem(string title)
     {
         Title = title;
+        CreatedAt = DateTime.Now;
         IsCompleted = false;
     }
-}
 
+    public event PropertyChangedEventHandler PropertyChanged;
+<<<<<<< HEAD
+
+    protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+=======
+>>>>>>> ecfcb6d406a6bc260cc3e57a899bb2427741552f
+}
